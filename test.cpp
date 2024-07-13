86364e6d8cca37735bc82190df01482a54712c71
canhdeptrai222 <158004919+canhdeptrai222@users.noreply.github.com>
2024-07-13 13:24:04 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
-
+#include <queue>
+#include <stack>
+#define MAX 100
 using namespace std;
+
+struct Stack {
+	int a[MAX];
+	int top;
+};
+void init(Stack& s) {
+	s.top = -1;
+}
+bool isEmpty(Stack s) {
+	return s.top < 0;
+}
+bool isFull(Stack s) {
+	return s.top == MAX;
+}
+void push(Stack& s, int x) {
+	s.a[++s.top] = x;
+}
+int pop(Stack& s) {
+	return s.a[s.top--];
+}
+struct Queue {
+	int a[MAX];
+	int front;
+	int rear;
+};
+void init(Queue& q) {
+	q.front = q.rear = -1;
+}
+bool isEmpty(Queue q) {
+	return q.front < 0;
+}
+bool isFull(Queue q) {
+	return q.rear == MAX;
+}
+void enQueue(Queue& q, int x) {
+	if (isEmpty(q) == true) {
+		q.a[++q.front] = x;
+		q.rear++;
+	}
+	else {
+		q.a[++q.rear] = x;
+	}
+}
+int deQueue(Queue& q) {
+	return q.a[q.front++];
+}
+
+
 
 struct Node
 {
@@ -8,12 +58,10 @@ struct Node
 	Node *left;
 	Node *right;
 };
-
 void init(Node *&root)
 {
 	root = NULL;
 }
-
 Node *createNode(int x)
 {
 	Node *p = new Node;
@@ -121,7 +169,6 @@ void searchLeftFinal(Node *&p)
 		return;
 	}
 }
-
 // khong de quy!!!!!!!!!!!
 void delNode(Node *&root, int x)
 {
@@ -244,7 +291,6 @@ int delNodeRecursion(Node *&root, int x)
 	if (root->value > x)
 		return delNodeRecursion(root->left, x);
 }
-
 Node* nodeMax(Node* root) {
 	if (root->right == NULL)
 		return root;
@@ -260,16 +306,107 @@ int countNode(Node* root) {
 	    return countNode(root->left) + 1 + countNode(root->right);*/
     else return countNode(root->left) + countNode(root->right) + 1;
 }
-//NO UNDERSTADNDING
-//int sum(Node* root) {
-//	if (root == NULL)
-//		return 0;
-//	int leftValue = sum(root->left);
-//	int rightValue = sum(root->right);
-//	return root->value + leftValue + rightValue;
-//}
-int sum(Node* root) {
+int countNodeBac2(Node* root) {
+	if (root == NULL)
+		return 0;
+	if (root->left != NULL && root->right != NULL)
+		return 1 + countNodeBac2(root->left) + countNodeBac2(root->right);
+	else
+		return countNodeBac2(root->left) + countNodeBac2(root->right);
+}
+void outputNode2Children(Node* root) {
+	if (root == NULL)
+		return ;
+	if (root->left != NULL && root->right != NULL) {
+		cout << root->value << " ";
+	}
+	outputNode2Children(root->left);
+	outputNode2Children(root->right);
+}
+void searchStack(Node* root, int x, Stack& s) {
+	Node* p = root;
+	while (p != NULL && p->value != x) {
+		if (x > p->value)
+		{
+			push(s, p->value);
+			p = p->right;
+		}
+		else {
+			push(s, p->value);
+			p = p->left;
+		}
+	}
+	if (p != NULL)
+		push(s, p->value);
+}
+void searchQueue(Node* root, int y, Queue& q) {
+	Node* p = root;
+	while (p != NULL && p->value != y) {
+		if (y > p->value)
+		{
+			enQueue(q, p->value);
+			p = p->right;
+		}
+		else {
+			enQueue(q, p->value);
+			p = p->left;
+		}
+	}
+	if (p != NULL)
+		enQueue(q, y);
+}
+void pathFromNodeAToB(Node* root, int x, int y) {
+	Stack s; init(s);
+	Queue q; init(q);
+	if (x > root->value && y > root->value) {
+		
+	}
+	else if (x < root->value && y < root->value) {
 
+	}
+	else {
+
+		searchStack(root, x, s);
+		searchQueue(root, y, q);
+		/*while (!isEmpty(s)) {
+			int step1 = pop(s);
+			int step2 = pop(s);
+			cout << "(" << step1 << ", " << step2 << ")" << endl;
+			push(s, step2);
+			if (s.top == 0)
+				break;
+		}
+		Queue tmp; init(tmp); int temp;
+		int step1 = deQueue(q);
+		int step2 = deQueue(q);
+		do {
+			cout << "(" << step1 << ", " << step2 << ")" << endl;
+			enQueue(tmp, step2);
+			step1 = deQueue(tmp);
+			step2 = deQueue(q);
+		} while (q.front <= q.rear + 1);*/
+		while (!isEmpty(s)) {
+			cout << pop(s) << "->";
+			if (s.top == 0)
+				break;
+		}
+		while (q.front <= q.rear) {
+			cout << deQueue(q);
+			if (q.front <= q.rear)
+				cout << "->";
+			else
+				break;
+		}
+	}
+
+}
+//NO UNDERSTADNDING???
+int sum(Node* root) {
+	if (root == NULL)
+		return 0;
+	int leftValue = sum(root->left);
+	int rightValue = sum(root->right);
+	return root->value + leftValue + rightValue;
 }
 void lnr(Node *root)
 {
@@ -298,6 +435,31 @@ void lrn(Node *root)
 		cout << root->value << " ";
 	}
 }
+/*//void nlrStack(Node* root,Stack s) {
+//	init(s);
+//	Node* p = root;
+//
+//}
+//void inorderStack(Node* root) {
+//	if (root == nullptr) return;
+//
+//	stack<Node*> s;
+//	Node* current = root;
+//
+//	while (s.size() > 0 || current != nullptr) {
+//		if (current != nullptr) {
+//			s.push(current);
+//			current = current->left;
+//		}
+//		else {
+//			current = s.top();
+//			s.pop();
+//			cout << current->value << " ";
+//			current = current->right;
+//		}
+//	}
+//}*/
+
 int main()
 {
 	Node *root;
@@ -318,12 +480,16 @@ int main()
 	addNode(root, 47);
 	addNode(root, 60);
 	addNode(root, 45);
-	// cout << countNode(root) << endl;
+	pathFromNodeAToB(root, 60, 45);
+	//nlr(root);
+	//cout << endl;
+	//inorderStack(root);
+	//cout << endl;
+	//cout << countNode(root) << endl;
 	/*addNodeRecursion(root, 40);
 	addNodeRecursion(root, 20);
 	addNodeRecursion(root, 50);*/
-	lnr(root);
-	cout << endl;
+	
 	//// cout << searchRecursion(root, 210)<< endl;
 	//cout << "=============DELETE============\n";
 	//delNodeRecursion(root, 40);
@@ -331,8 +497,10 @@ int main()
 	//cout << endl;
 	/*cout << "========MAX===========\n";
 	cout << nodeMax(root)->value << endl;*/
-	cout << "===============AVERAGE===============\n"; //31
+	//cout << "===============AVERAGE===============\n"; //31
 	/*int tb = sum(root) / countNode(root);
 	cout << tb << endl; */
+	/*cout << countNodeBac2(root) << endl;
+	outputNode2Children(root);*/
 	return 0;
 }
