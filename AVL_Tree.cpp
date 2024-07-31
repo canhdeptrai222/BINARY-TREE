d758780e18fdf610fd6cd50b567bc1df55c8f299
canhdeptrai222 <158004919+canhdeptrai222@users.noreply.github.com>
2024-07-31 07:50:56 +0700

    Update AVL_Tree.cpp

--- AVL_Tree.cpp
+++ AVL_Tree.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stack>
 #include <queue>
 #include <math.h>
@@ -11,12 +11,8 @@ struct Node {
 	int height;
 };
 
-struct AVLTree {
-	Node* root;
-};
-
-void init(AVLTree& r) {
-	r.root = NULL;
+void init(Node*& root) {
+	root = NULL;
 }
 
 Node* createNode(int x) {
@@ -33,193 +29,25 @@ int height(Node* root) {
 		return 0;
 	return root->height;
 }
-
-Node* rightRotate(Node* y) {
-    Node* x = y->left;
-    Node* T2 = x->right;
-
-    // Perform rotation
-    x->right = y;
-    y->left = T2;
-
-    // Update heights
-    y->height = max(height(y->left), height(y->right)) + 1;
-    x->height = max(height(x->left), height(x->right)) + 1;
-
-    // Return new root
-    return x;
+int max(int a, int b) {
+	return a > b ? a : b;
 }
-
-Node* leftRotate(Node* x) {
-    Node* y = x->right;
-    Node* T2 = y->left;
-
-    // Perform rotation
-    y->left = x;
-    x->right = T2;
-
-    // Update heights
-    x->height = max(height(x->left), height(x->right)) + 1;
-    y->height = max(height(y->left), height(y->right)) + 1;
-
-    // Return new root
-    return y;
+void rightRotate(Node*& root) {
+	Node* x = root->left;
+	root->left = x->right;
+	x->right = root;
+	root->height = max(height(root->left), height(root->right)) + 1;
+	x->height = max(height(x->left), height(x->right)) + 1;
+	root = x;
 }
-
-int getBalance(Node* node) {
-    if (node == NULL)
-        return 0;
-    return height(node->left) - height(node->right);
+void leftRotate(Node*& root) {
+	Node* x = root->right;
+	root->right = x->left;
+	x->left = root;
+	root->height = max(height(root->left), height(root->right)) + 1;
+	x->height = max(height(x->left), height(x->right)) + 1;
+	root = x;
 }
-
-Node* insert(Node* node, int key) {
-    /* 1. Perform normal BST insertion */
-    if (node == NULL)
-        return createNode(key);
-
-    if (key < node->value)
-        node->left = insert(node->left, key);
-    else if (key > node->value)
-        node->right = insert(node->right, key); 
-
-    else // Equal keys not allowed
-        return node;
-
-    /* 2. Update height   
- */
-    node->height = 1 + max(height(node->left), height(node->right));
-
-    /* 3. Get the balance factor of this ancestor */
-    int balance = getBalance(node); 
-
-
-    // Balancing using a loop to avoid recursion
-    while (abs(balance) > 1) {
-        // Left Left Case
-        if (balance > 1 && key < node->left->value) {
-            node = rightRotate(node);
-            balance = getBalance(node);
-        }
-
-        // Right Right Case
-        if (balance < -1 && key > node->right->value) {
-            node = leftRotate(node);
-            balance = getBalance(node);
-        }
-
-        // Left Right Case
-        if (balance > 1 && key > node->left->value) {
-            node->left = leftRotate(node->left);
-            node = rightRotate(node);
-            balance = getBalance(node);
-        }
-
-        // Right Left Case
-        if (balance < -1 && key < node->right->value) {
-            node->right = rightRotate(node->right);
-            node = leftRotate(node);
-            balance = getBalance(node);
-        }
-    }
-
-    /* return the (unchanged) node pointer */
-    return node;
-}
-
-
-//int max(int a, int b) {
-//	return a > b ? a : b;
-//}
-//Node* max(Node* a, Node* b) {
-//	if (a->height > b->height)
-//		return a;
-//	return b;
-//}
-//bool getBalance(Node* q) {
-//	if (abs(height(q->left) - height(q->right)) > 1)
-//		return false;
-//	return true;
-//}
-//Node* checkBalance(AVLTree r) {
-//	queue<Node*> q;
-//	Node* p = r.root;
-//	q.push(p);
-//	while (!q.empty()) {
-//		Node* cur = q.front();
-//		q.pop();
-//		if (!getBalance(cur))
-//			return cur;
-//		if (cur->left != NULL)
-//			q.push(cur->left);
-//		if (cur->right != NULL)
-//			q.push(cur->right);
-//	}
-//	return NULL;
-//}
-//void rightRotate(Node*& tmp, Node* parent) {
-//	Node* p = tmp;
-//	Node* q = tmp->left;
-//	if (parent == NULL) {
-//		p->left = q->right;
-//		q->right = p;
-//	}
-//	else {
-//		p->left = q->right;
-//		q->right = p;
-//		if (tmp->value < parent->value)
-//			parent->left = q;
-//		else
-//			parent->right = q;
-//	}
-//	tmp->height = max(height(p->left), height(p->right)) + 1;
-//	q->height = max(height(q->left), height(q->right)) + 1;
-//}
-//void addNode(AVLTree& r, int x) {
-//	stack<Node*> update;
-//	stack<Node*> check;
-//	if (r.root == NULL)
-//		r.root = createNode(x);
-//	else {
-//		Node* p = r.root;
-//		Node* parent = NULL;
-//		while (p != NULL && p->value != x) {
-//			update.push(p);
-//			parent = p;
-//			//p->height = max(height(parent->left), height(parent->right)) + 1;
-//			if (x > p->value)
-//				p = p->right;
-//			else
-//				p = p->left;
-//		}
-//		if (p == NULL) {
-//			if (x > parent->value)
-//				parent->right = createNode(x);
-//			else
-//				parent->left = createNode(x);
-//			//update height
-//			while (!update.empty()) {
-//				update.top()->height = max(height(update.top()->left), height(update.top()->right)) + 1;
-//				check.push(update.top());
-//				update.pop();
-//			}
-//			//cout << 1 << endl;
-//			Node* tmp = NULL;
-//			while (!check.empty()) {
-//				if (getBalance(check.top()) == false) {
-//					tmp = check.top();
-//					check.pop();
-//					break;
-//				}
-//				check.pop();
-//			}
-//			if (tmp != NULL) {
-//				rightRotate(tmp, check.top());
-//			}
-//		}
-//		
-//	}
-//}
-
 bool isBalanced(Node* node) {
 	// Cây rỗng hoặc cây chỉ có một nút luôn cân bằng
 	if (node == NULL || (node->left == NULL && node->right == NULL)) {
@@ -240,39 +68,198 @@ bool isBalanced(Node* node) {
 	return false;
 
 }
-
-void addNode(AVLTree& r, int x) {
-    r.root = insert(r.root, x);
-}
-
-
-Node* search(Node* root, int x)
-{
-	Node* p = root;
-	while (p != NULL && p->value != x)
-	{
-		if (x > p->value)
-			p = p->right;
-		else
-			p = p->left;
+void makeBalance(Node*& root, int x) {
+	int balance = height(root->left) - height(root->right);
+	if (balance > 1 && root->left->value > x) {
+		rightRotate(root);
 	}
-	return p;
+	else if (balance < -1 && root->right->value < x) {
+		leftRotate(root);
+	}
+	else if (balance > 1 && root->left->value < x) {
+		leftRotate(root->left);
+		rightRotate(root);
+	}
+	else if (balance < -1 && root->right->value > x) {
+		rightRotate(root->right);
+		leftRotate(root);
+	}
+}
+int getBalance(Node* root) {
+	return height(root->left) - height(root->right);
+}
+void addNode(Node*& root, int x) {
+	if (root == NULL)
+		root = createNode(x);
+	else {
+		if (x > root->value)
+			addNode(root->right, x);
+		else if (x < root->value)
+			addNode(root->left, x);
+		else
+			return;
+		root->height = 1 + max(height(root->left), height(root->right));
+		makeBalance(root, x);
+	}
+}
+void leftNodeFinal(Node*& p, Node*& q) {
+	if (q->left == NULL) {
+		p->value = q->value;
+		p = q;
+		q = q->right;
+	}
+	else
+		leftNodeFinal(p, q->left);
+}
+void delNode(Node*& root, int x) {
+	if (root == NULL)
+		return;
+	else {
+		if (x > root->value)
+			delNode(root->right, x);
+		else if (x < root->value)
+			delNode(root->left, x);
+		else if (x == root->value) {
+			//luu node can xoa
+			Node* p = root;
+			if (root->left == NULL)
+				root = root->right;
+			else if (root->right == NULL)
+				root = root->left;
+			else
+				leftNodeFinal(p, root->right);
+			delete p;
+			p = NULL;
+		}
+		if (root != NULL) {
+			root->height = 1 + max(height(root->left), height(root->right));
+			int balance = getBalance(root);
+			if (balance > 1 && getBalance(root->left) >= 0) {
+				rightRotate(root);
+			}
+			else if (balance < -1 && getBalance(root->right) <= 0) {
+				leftRotate(root);
+			}
+			else if (balance > 1 && getBalance(root->left) <= 0) {
+				leftRotate(root->left);
+				rightRotate(root);
+			}
+			else if (balance < -1 && getBalance(root->right) >= 0) {
+				rightRotate(root->right);
+				leftRotate(root);
+			}
+		}
+	}
+}
+void BFS(Node* root) {
+	if (root == NULL)
+		return;
+	queue<Node*> q;
+	Node* p = root;
+	q.push(p);
+	while (!q.empty()) {
+		Node* cur = q.front();
+		cout << cur->value << "(" << cur->height << ") ";
+		q.pop();
+		if (cur->left != NULL) {
+			q.push(cur->left);
+		}
+		if (cur->right != NULL) {
+			q.push(cur->right);
+		}
+	}cout << endl;
+}
+void deleteALLNode(Node*& root) {
+	if (root == NULL)
+		return;
+	deleteALLNode(root->left);
+	deleteALLNode(root->right);
+	delete root;
+	root = NULL;
 }
 int main() {
-	Node* r = NULL;
-	AVLTree root;
+	Node* root;
 	init(root);
-	addNode(root, 40);
-	addNode(root, 20);
-	addNode(root, 60);
-	addNode(root, 25);
-	addNode(root, 15);
+	//case 1
+	/*addNode(root,20);
+	addNode(root, 4);
+	addNode(root, 26);
+	addNode(root, 3);
+	addNode(root, 9);*/
+	//before add 20(3) 4(2) 26(1) 3(1) 9(1)
+	//addNode(root, 15);
+	//after add 9(3) 4(2) 20(2) 3(1) 15(1) 26(1)
+	//case 2
+	/*addNode(root,20);
+	addNode(root, 4);
+	addNode(root, 26);
+	addNode(root, 3);
+	addNode(root, 9);
+	addNode(root, 21);
+	addNode(root, 30);
+	addNode(root, 2);
+	addNode(root, 7);
+	addNode(root, 11);*/
+	//before add 20(4) 4(3) 26(2) 3(2) 9(2) 21(1) 30(1) 2(1) 7(1) 11(1)
+	//addNode(root, 15);
+	//after add 9(4) 4(3) 20(3) 3(2) 7(1) 11(2) 26(2) 2(1) 15(1) 21(1) 30(1)
+	//case 3
+	/*addNode(root, 2);
+	addNode(root, 1);
+	addNode(root, 4);
+	addNode(root, 3);
+	addNode(root, 5);
+	delNode(root, 1);*/
+	//res = 4(3) 2(2) 5(1) 3(1)
+	//case 4
+	/*addNode(root, 6);
+	addNode(root, 9);
+	addNode(root, 2);
+	addNode(root, 1);
+	addNode(root, 4);
+	addNode(root, 8);
+	addNode(root, 11);
+	addNode(root, 3);
+	addNode(root, 5);
+	addNode(root, 7);
 	addNode(root, 10);
-	if (isBalanced(root.root))
-		cout << "balance" << endl;
-	else
-		cout << "not balance" << endl;
-	cout << search(root.root, 40)->height << endl;
-	cout << search(root.root, 20)->height << endl;
+	addNode(root, 12);
+	addNode(root, 13);
+	delNode(root, 1);*/
+	//res = 6(5) 4(3) 9(4) 2(2) 5(1) 8(2) 11(3) 3(1) 7(1) 10(1) 12(2) 13(1)
+	//case 5
+	/*addNode(root, 5);
+	addNode(root, 2);
+	addNode(root, 8);
+	addNode(root, 1);
+	addNode(root, 3);
+	addNode(root, 7);
+	addNode(root, 10);
+	addNode(root, 4);
+	addNode(root, 6);
+	addNode(root, 9);
+	addNode(root, 11);
+	addNode(root, 12);
+	delNode(root, 1);*/
+	//res = 8(4) 5(3) 10(3) 3(2) 7(2) 9(1) 11(2) 2(1) 4(1) 6(1) 12(1)
+	//case 6
+	addNode(root, 8);
+	addNode(root, 3);
+	addNode(root, 10);
+	addNode(root, 2);
+	addNode(root, 5);
+	addNode(root, 9);
+	addNode(root, 12);
+	addNode(root, 1);
+	addNode(root, 4);
+	addNode(root, 6);
+	addNode(root, 11);
+	addNode(root, 7);
+	delNode(root, 10);
+	/*if (isBalanced(root))
+		cout << "1";*/
+	BFS(root);
+	deleteALLNode(root);
+	BFS(root);
 	return 1;
 }
